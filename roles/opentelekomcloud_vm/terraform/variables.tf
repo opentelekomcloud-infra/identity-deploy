8c6fbf227166210349fab87553eff997763dd0b0
Anton Kachurin <anton.kachurin@t-systems.com>
2021-12-17 14:48:52 +0300

    Add `otc` VM provider (#5)
    Allow provisioning OTC instance using `terraform`

---
+++ roles/opentelekomcloud_vm/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "keypair_name" {}
+variable "router_name" {}
+variable "cidr" {}
+variable "subnet_name" {}
+variable "flavor" { default = "s2.xlarge.4" }
+variable "image" {}
+variable "server_name" {}
+variable "sg_name" {}
+variable "az" { default = "eu-de-01" }
+variable "public_key" {}
